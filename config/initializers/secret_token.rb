80045ae4e6700c5c7e699aaffdb8144a1ee29779
Apprentice <apprentice@dbc09.local>
2013-03-13 10:33:27 -0700

    Initial commit without minitest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackUnderflow::Application.config.secret_token = '75908a83b4a0fee69cac3a81edbf595ea6e6067f844862f4df234337ba7634bf93efb88e56cb76f1dfe3f3c9dea60d2c120bfd724014d92fcfea07c67eefbf97'
